f5c6fc62fcbaa6aa6f69d5c3c76938992c90091f
YasminMoawad <Yasminemohamed811@gmail.com>
2022-05-26 15:53:47 +0200

    add project

---
+++ src/InvoiceFrame2/main_invoice.java
@@ -0,0 +1,14 @@
+package InvoiceFrame2;
+
+public class main_invoice {
+
+    public static void main (String[] args) {
+
+        InvoiceJframe f = new InvoiceJframe();
+         f.setVisible(true);
+
+
+
+
+    }
+}
